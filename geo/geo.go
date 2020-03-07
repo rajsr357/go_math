408aa0143b318f8a68eec96c9f3f6f52f12a9ce7
rajsr357 <rajsr357@gmail.com>
2020-03-07 16:09:12 -0600

    zero patch not allowed

--- geo/geo.go
+++ geo/geo.go
@@ -1,5 +1,12 @@
 package geo
 
-func CubeVolume(n int) int {
-	return n * n * n
+import "errors"
+
+func CubeVolume(n int) (int,error) {
+	if n!=0{
+	return n * n * n,nil
+}else{
+	return 0, errors.New("Zero Value not valid")
+}
+
 }
